325140d5169fa686a6a82c4d86ea0885dcab9230
Carlo :) <carlorosso02@gmail.com>
2025-10-31 10:09:44 +0100

    changing L2 to L1

--- src/atdl_sws/models/lenet_300.py
+++ src/atdl_sws/models/lenet_300.py
@@ -1,6 +1,6 @@
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Flatten, Input
-from tensorflow.keras.regularizers import L2
+from tensorflow.keras.regularizers import L1
 
 
 def lenet_300_100(num_classes=10, regularizer=0):
@@ -8,8 +8,8 @@ def lenet_300_100(num_classes=10, regularizer=0):
         [
             Input(shape=(28, 28)),
             Flatten(),
-            Dense(300, activation="relu", kernel_regularizer=L2(regularizer)),
-            Dense(100, activation="relu", kernel_regularizer=L2(regularizer)),
+            Dense(300, activation="relu", kernel_regularizer=L1(regularizer)),
+            Dense(100, activation="relu", kernel_regularizer=L1(regularizer)),
             Dense(num_classes, activation="softmax"),
         ]
     )
--- src/atdl_sws/models/lenet_caffe.py
+++ src/atdl_sws/models/lenet_caffe.py
@@ -1,6 +1,6 @@
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Flatten, Input, Conv2D, MaxPooling2D
-from tensorflow.keras.regularizers import L2
+from tensorflow.keras.regularizers import L1
 
 
 def lenet_caffe(num_classes=10, regularizer=0):
@@ -8,15 +8,15 @@ def lenet_caffe(num_classes=10, regularizer=0):
         [
             Input(shape=(28, 28, 1)),
             Conv2D(
-                20, kernel_size=5, activation="relu", kernel_regularizer=L2(regularizer)
+                20, kernel_size=5, activation="relu", kernel_regularizer=L1(regularizer)
             ),
             MaxPooling2D(pool_size=2),
             Conv2D(
-                50, kernel_size=5, activation="relu", kernel_regularizer=L2(regularizer)
+                50, kernel_size=5, activation="relu", kernel_regularizer=L1(regularizer)
             ),
             MaxPooling2D(pool_size=2),
             Flatten(),
-            Dense(500, activation="relu", kernel_regularizer=L2(regularizer)),
+            Dense(500, activation="relu", kernel_regularizer=L1(regularizer)),
             Dense(num_classes, activation="softmax"),
         ]
     )
